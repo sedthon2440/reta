bb0c9860db10d043814691630f3b08ce9376952b
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-04-09 04:57:30 +0300

    Update Adisa.py

--- AarohiX/plugins/Management/Adisa.py
+++ AarohiX/plugins/Management/Adisa.py
@@ -85,7 +85,7 @@ channel = ["القناة"]
 
 # ========================================= #
 
-@app.on_message(filters.command(["فهد", "فهود"], prefixes=["a", "A"]) & admin_filter)
+@app.on_message(filters.command(["ريتا", "رتو"], prefixes=["a", "A"]) & admin_filter)
 async def restriction_app(app: app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
