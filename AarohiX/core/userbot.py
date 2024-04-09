269fe34c55b6386d2dcd8d34075d335ab94a6575
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-04-09 05:52:45 +0300

    Update userbot.py

--- AarohiX/core/userbot.py
+++ AarohiX/core/userbot.py
@@ -54,8 +54,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("WX_PM")
-                await self.one.join_chat("T7_AU")
+                await self.one.join_chat("veevvw")
+                await self.one.join_chat("sedthon_help")
             except:
                 pass
             assistants.append(1)
@@ -83,8 +83,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Tepthon")
-                await self.two.join_chat("Tepthon_Help")
+                await self.two.join_chat("veevvw")
+                await self.two.join_chat("sedthon_Help")
             except:
                 pass
             assistants.append(2)
@@ -112,8 +112,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Tepthon")
-                await self.three.join_chat("Tepthon_Help")
+                await self.three.join_chat("veevvw")
+                await self.three.join_chat("sedthon_Help")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Tepthon")
-                await self.four.join_chat("Tepthon_Help")
+                await self.four.join_chat("veevvw")
+                await self.four.join_chat("sedthon_Help")
             except:
                 pass
             assistants.append(4)
@@ -170,8 +170,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Tepthon")
-                await self.five.join_chat("Tepthon_Help")
+                await self.five.join_chat("veevvw")
+                await self.five.join_chat("sedthon_Help")
             except:
                 pass
             assistants.append(5)
