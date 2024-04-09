55e6859afa5f2248bc014f526f9654ff74934e6a
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-04-09 05:15:08 +0300

    Update Gpt.py

--- AarohiX/plugins/Management/Gpt.py
+++ AarohiX/plugins/Management/Gpt.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}      انضم هُنا لُطفًا › @Tepthon",
+                        f" {x}      انضم هُنا لُطفًا › @VEEVVW",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
