d9bf2188e4145b645478ee882317835d12692f26
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-04-15 15:51:48 +0300

    Update must_join.py

--- AarohiX/plugins/play/must_join.py
+++ AarohiX/plugins/play/must_join.py
@@ -5,23 +5,23 @@ from AarohiX import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/Tepthon":  # Not compulsory
+    if not "https://t.me/veevvw":  # Not compulsory
         return
     try:
         try:
             await bot.get_chat_member("Tepthon", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/Tepthon".isalpha():
-                link = "https://t.me/Tepthon"
+            if "https://t.me/veevvw".isalpha():
+                link = "https://t.me/veevvw"
             else:
-                chat_info = await bot.get_chat("Tepthon")
+                chat_info = await bot.get_chat("veevvw")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذرًا عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت أولًا\n⌯︙قناة البوت : @Tepthon .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذرًا عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت أولًا\n⌯︙قناة البوت : @veevvw .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("تحديثات فهد 🇵🇸", url=link)]
+                        [InlineKeyboardButton("تحديثات ريتا 🇵🇸", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
